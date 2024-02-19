1883b7488fbd3d6bc5c8f59709fa68610314b8ce
darlison-calm <bernardo.darlison@gmail.com>
2024-02-19 18:19:20 -0300

    Create playGame function

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const computerSelection = getComputerChoice();
-const playerSelection = prompt('Rock , Paper, Scissors: ');
+let playerScore = 0
+let computerScore = 0
 
-const round = playRound(playerSelection, computerSelection);
-
-console.log(round)
-
+playGame()
 
 
 function getComputerChoice() {
@@ -18,20 +15,20 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // your code here!
-    const playerChoice = playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+    const playerChoice = playerSelection
 
     if (playerChoice === 'Rock' && computerSelection === 'Scissors'){
-        return('You win! Rock beats Scissors');
+        return('You win the round! Rock beats Scissors');
 
     }
 
     else if(playerChoice ==='Paper' && computerSelection === 'Rock') {
-        return('You win! Paper beats Rock');
+        return('You win the round! Paper beats Rock');
 
     }
 
     else if(playerChoice === 'Scissors' && computerSelection=== 'Paper') {
-        return('You win! Scissors beats Paper');
+        return('You win the round! Scissors beats Paper');
     }
 
     else if (playerChoice===computerSelection) {
@@ -39,7 +36,52 @@ function playRound(playerSelection, computerSelection) {
     }
 
     else {
-        return("Computer Wins! " + computerSelection + ' beats ' + playerChoice);
+        return('Computer wins the round! ' + computerSelection + ' beats ' + playerChoice);
     }
   }
+
+function playGame() {
+
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = prompt('Rock , Paper, Scissors: ');
+        
+        playerSelection.charAt(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
+        
+        let roundWinner = playRound(playerSelection, computerSelection);
+
+        console.log('Computer: ' + computerSelection , 'Player: ' + playerSelection) 
+
+        console.log(roundWinner)
+
+        if (roundWinner.startsWith('You win')) {
+            playerScore++
+            console.log('You ' + playerScore)
+        }
+
+        else if (roundWinner.startsWith('Computer wins') ){
+            computerScore++
+            console.log('Computer ' + computerScore)
+
+        }
+
+        
+
+    }
+
+    if(computerScore > playerScore) {
+        console.log("Computer wins the game")
+    }
+
+    else if (playerScore > computerScore) {
+        console.log('You win the game!')
+    }
+
+    else {
+        console.log("The game ends in a draw!")
+    }
+
+    console.log("Computer Score: " + computerScore, "Player Score: " + playerScore)
+
+}
 
